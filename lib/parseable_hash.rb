c12e7cb23ca4fef7330c1b98dd2c55663bc03e9b
Konrad Oleksiuk <konrad@up-next.com>
2013-12-25 23:25:25 +0100

    init basic structure for class module

--- lib/parseable_hash.rb
+++ lib/parseable_hash.rb
@@ -1,5 +1,30 @@
 require "parseable_hash/version"
 
 module ParseableHash
-  # Your code goes here...
+  def self.included(klass)
+    klass.send(:include, InstanceMethods)
+    klass.extend(ClassMethods)
+  end
+
+  module InstanceMethods
+    def parse_hash(object, options = {})
+      parse_with_strategy(object, options[:with])
+    end
+
+    private
+
+    def parse_with_strategy(hash, strategry = :default)
+      hash
+    end
+  end
+
+  module ClassMethods
+    def parse_strategy(name, options = {})
+      parse_strategies[name] = options
+    end
+
+    def parse_strategies
+      @parse_strategies ||= {}
+    end
+  end
 end
---
+++ spec/dummy_object_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+class Dummy
+  include ParseableHash
+
+  parse_strategy :random_values, int: :integer, fl: :float, nested: { dt: :date }
+  parse_strategy :int, int: :integer
+
+  def initialize(options = {})
+    @options = options
+  end
+
+  def call
+    parse_hash(@options, with: :random_values)
+  end
+end
+
+describe Dummy do
+  context 'when all values exist' do
+    let(:str_date) { '12.12.2013' }
+    let(:obj_date) { Date.parse(str_date) }
+    let(:obj) { Dummy.new(int: '3', fl: '3.5', nested: { dt: str_date }) }
+
+    let(:parsed_hash) { obj.call }
+
+    it 'should parse hash to values passed with given strategry' do
+      expect(parsed_hash).to eq(int: 3, fl: 3.5, nested: { dt: obj_date })
+    end
+  end
+end
