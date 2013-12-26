694012cded9c3fb760b1a9ff51ce06d30ea32395
Konrad Oleksiuk <konrad@up-next.com>
2013-12-26 13:51:36 +0100

    Use 1.8.7 style for hashes

--- spec/dummy_object_spec.rb
+++ spec/dummy_object_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 class Dummy
   include ParseableHash
 
-  parse_strategy :random_values, int: :integer, fl: :float, nested: { dt: :date }
-  parse_strategy :int, int: :integer
+  parse_strategy :random_values, :int => :integer, :fl => :float, :nested => { :dt => :date }
+  parse_strategy :int, :int => :integer
 
   def initialize(options = {})
     @options = options
   end
 
   def call
-    parse_hash(@options, with: :random_values)
+    parse_hash(@options, :with => :random_values)
   end
 end
 
@@ -19,13 +19,13 @@ describe Dummy do
   context 'when all values exist' do
     let(:str_date) { '12.12.2013' }
     let(:obj_date) { Date.parse(str_date) }
-    let(:obj) { Dummy.new(int: '3', fl: '3.5', nested: { dt: str_date }) }
+    let(:obj) { Dummy.new(:int => '3', :fl => '3.5', :nested => { :dt => str_date }) }
 
     let(:parsed_hash) { obj.call }
 
     it 'should parse hash to values passed with given strategy' do
       pending
-      #expect(parsed_hash).to eq(int: 3, fl: 3.5, nested: { dt: obj_date })
+      #expect(parsed_hash).to eq(:int => 3, :fl => 3.5, :nested => { :dt => obj_date })
     end
   end
 end
